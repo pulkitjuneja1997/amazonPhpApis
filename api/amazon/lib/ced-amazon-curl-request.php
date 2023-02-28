a6e0e147cf3b19b1e6bda0ba09ce6286f13591d1
pulkitjuneja1997 <pulkitjuneja1997@gmail.com>
2023-02-28 21:16:23 +0530

    6 commit

--- api/amazon/lib/ced-amazon-curl-request.php
+++ api/amazon/lib/ced-amazon-curl-request.php
@@ -2,6 +2,8 @@
 
 class Ced_Amazon_Curl_Request {
 
+	public $upload_dir;
+
 	public function ced_amazon_get_category( $url ) {
 
 		$access_token = get_option( 'ced_amazon_sellernext_access_token', true );
@@ -38,32 +40,39 @@ class Ced_Amazon_Curl_Request {
 
 	public function fetchProductTemplate( $category_id, $userCountry ) {
 
+		$this->upload_dir = __DIR__ . '/uploads';
 		// Product flat file template structure json file
 		$file_location = 'lib/' . $userCountry . '/' . $category_id . '/json/products_template_fields.json';  // products_all_fields.json'
 		$endpoint      = 'https://lo9bsyugeh.execute-api.ap-southeast-1.amazonaws.com/webapi/amazon/get_template';
 		$body          = array(
 			'location' => $file_location,
 		);
-		$body          = wp_json_encode( $body );
+		$body          = json_encode( $body );
 
-		$options = array(
-			'body'        => $body,
-			'headers'     => array(
-				'Content-Type' => 'application/json',
-			),
-			'timeout'     => 200,
-			'httpversion' => '1.0',
-			'sslverify'   => false,
-		);
+		// $options = array(
+		// 	'body'        => $body,
+		// 	'headers'     => array(
+		// 		'Content-Type' => 'application/json',
+		// 	),
+		// 	'timeout'     => 200,
+		// 	'httpversion' => '1.0',
+		// 	'sslverify'   => false,
+		// );
 
 		$data_response      = wp_remote_post( $endpoint, $options );
+
+		$data_response = $client->post( $endpoint , [
+			'headers' => ['Content-Type' => 'application/json'],
+			'body' => $body
+		]);
+
+		
 		$data_response      = json_decode( $data_response['body'], true );
 		$json_url           = $data_response['url'];
 		$json_url           = stripslashes( $json_url );
 		$json_template_data = file_get_contents( $json_url );
 
-		$upload_dir     = wp_upload_dir();
-		$dirname        = $upload_dir['basedir'] . '/ced-amazon/templates/' . $userCountry . '/' . $category_id;
+		$dirname        = $this->upload_dir . '/ced-amazon/templates/' . $userCountry . '/' . $category_id;
 		$json_file_name = 'products_template_fields.json';
 
 		if ( ! file_exists( $dirname . '/' . $json_file_name ) ) {

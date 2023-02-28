f5063d4dd40e0190b29fa03ba1ec7e820be7655d
pulkitjuneja1997 <pulkitjuneja1997@gmail.com>
2023-02-28 18:29:26 +0530

    5 commit

---
+++ api/api1/ced_amazon_core_functions.php
@@ -0,0 +1,35 @@
+<?php
+
+
+function getUserAccountMetaData(){
+
+    $url = $domain . '/wp-json/api-test/v1/userMetaData';
+    $args = array(
+        'method'      => 'GET',
+        'timeout'     => 45,
+        'sslverify'   => false,
+        'headers'     => array(
+            'Content-Type'  => 'application/json',
+        )
+        
+    );
+    $args = array();
+    $request = wp_remote_get( $url, $args );
+
+    if ( is_wp_error( $request ) || wp_remote_retrieve_response_code( $request ) != 200 ) {
+        error_log( print_r( $request, true ) );
+    }
+
+    $response = wp_remote_retrieve_body( $request );
+    $response = json_decode($response, true);
+
+    if( $response['status'] ){
+        return $response;
+    } else
+        return json_encode( array( 'status' => false, 'message' => ' '));
+    
+
+}
+
+
+?>
